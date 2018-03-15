3f9ac53abd660cae16655f4a23143b3280bc71ac
dupadhyay3 <upadhyaydeepak698@gmail.com>
2018-03-15 13:41:57 +0530

    13th Commit

--- includes/clientHeaderCart.php
+++ includes/clientHeaderCart.php
@@ -4,6 +4,6 @@
         e-Commerce Portal
     </span>
     <div id="headerUser" class="header-cart">Profile </div>
-    <div id="headerCart" class="header-cart"></div>
+    <div id="headerCart" class="header-cart"><i class="fa fa-shopping-cart"></i> Cart <span class="itemCount">0</span></div>
     <div class="clear"></div>
 </header>
